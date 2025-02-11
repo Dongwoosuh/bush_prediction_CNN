ad212bc570839b9836a18543bd82be3144e8c43f
DongwooSuh <dws1203@gmail.com>
2025-02-11 23:26:50 +0900

    inference도 case study가능하게 수정정

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import torch
 import json
 import pickle
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from network import build
 from source import *
@@ -31,14 +32,16 @@ def load_model_and_scalers(model_file: str, num_DV: int, device):
     
     return model, input_scaler, output_scalers
 
-def test_model(model, device, input_scaler, output_scalers, test_dataset, gt_dataset, field_range: int = 256, save_path: str = None):
+def test_model(model, device, input_scaler, output_scalers, test_dataset, gt_dataset, target_bush, field_range: int = 256, save_path: str = None):
     predictions = [] 
     predictions_post = []
     
     gt_dataset = gt_dataset.original_output_gt
     
-    wmape1_acc_total = 0
-    wmape2_acc_total = 0
+    _ , bush_idx = target_bush
+
+    result_df = pd.DataFrame(columns=["stiffness_num", "40%", "50%", "60%", "70%", "80%","100%"])
+    
     for idx, (input_data, _) in enumerate(test_dataset):
         
         input_data_unscaled = input_scaler.inverse_transform(input_data)
@@ -61,10 +64,6 @@ def test_model(model, device, input_scaler, output_scalers, test_dataset, gt_dat
         grid_x, grid_y = np.meshgrid(np.linspace(0, sub_axes_inverse, 16),
                                     np.linspace(0, main_axes_inverse, 16))
         
-        grid_x1, grid_y1 = np.meshgrid(np.linspace(0, sub_axes_inverse/2, 8),
-                                    np.linspace(0, main_axes_inverse/2, 8))
-
-
         train_X = np.column_stack([grid_x.ravel(), grid_y.ravel()])
         
         optimal_degree = loocv_optimization(train_X, prediction[0,:,:].flatten())
@@ -73,50 +72,78 @@ def test_model(model, device, input_scaler, output_scalers, test_dataset, gt_dat
         Z_pred = predict_on_grid(poly_model, poly, train_X)
         Z_pred = Z_pred.reshape( 16, 16)
         
-        
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(grid_x1, grid_y1, gt_dataset[idx][:8,:8], color='blue', alpha=0.5, label='Actual_50[%]')
-        ax.plot_surface(grid_x1, grid_y1, Z_pred[:8,:8], color='yellow', alpha=0.5, label='predicted-extrapolated_50[%]')
-        ax.set_xlabel('SubAxes')
-        ax.set_ylabel('MainAxes')
-        ax.set_zlabel('Value')
-        plt.legend()
-        folder_path = os.path.join(save_path, 'test_result_plot2')
-        file_name = os.path.join(folder_path, f"test_result_plot_{idx}_post_50%.png")
-        plt.savefig(file_name, dpi=300)
-        print(f"Saved: {file_name}")
+        pred_percentages = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        wmape1_acc_total = 0
+        wmape2_acc_total = 0
+        wmape_per_percent_list = []
+        wmape_full_range_list = []
+        for pred_percengtage in pred_percentages:
+          
+          grid_x1, grid_y1 = np.meshgrid(np.linspace(0, sub_axes_inverse*pred_percengtage, int(16*pred_percengtage)),
+                                      np.linspace(0, main_axes_inverse*pred_percengtage, int(16*pred_percengtage)))
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(grid_x1, grid_y1, gt_dataset[idx][:8,:8], color='blue', alpha=0.5, label='Actual_50[%]')
-        ax.plot_surface(grid_x1, grid_y1, prediction.reshape(16,16)[:8,:8], color='yellow', alpha=0.5, label='predicted-extrapolated_50[%]')
-        ax.set_xlabel('SubAxes')
-        ax.set_ylabel('MainAxes')
-        ax.set_zlabel('Value')
-        plt.legend()
-        folder_path = os.path.join(save_path, 'test_result_plot2')
-        file_name = os.path.join(folder_path, f"test_result_plot_{idx}_50%.png")
-        plt.savefig(file_name, dpi=300)
-        print(f"Saved: {file_name}")
+
+          
+          
+          folder_path_ = os.path.join(save_path, f'test_result_plot_bush_{bush_idx}')
+          
+          if not os.path.exists(folder_path_):
+              os.makedirs(folder_path_)
+              
+          folder_path = os.path.join(folder_path_, f'{pred_percengtage*100}%')
+          if not os.path.exists(folder_path):
+              os.makedirs(folder_path)
+              
+          fig = plt.figure()
+          ax = fig.add_subplot(111, projection='3d')
+          ax.plot_surface(grid_x1, grid_y1, gt_dataset[bush_idx*6 + idx][:int(16*pred_percengtage),:int(16*pred_percengtage)], color='blue', alpha=0.5, label='Actual_50[%]')
+          ax.plot_surface(grid_x1, grid_y1, Z_pred[:int(16*pred_percengtage),:int(16*pred_percengtage)], color='yellow', alpha=0.5, label='predicted-extrapolated_50[%]')
+          ax.set_xlabel('SubAxes')
+          ax.set_ylabel('MainAxes')
+          ax.set_zlabel('Value')
+          plt.legend()
+          file_name = os.path.join(folder_path, f"test_result_plot_{idx}_post_50%.png")
+          plt.savefig(file_name, dpi=300)
+          print(f"Saved: {file_name}")
+
+          fig = plt.figure()
+          ax = fig.add_subplot(111, projection='3d')
+          ax.plot_surface(grid_x, grid_y, gt_dataset[bush_idx*6 + idx], color='blue', alpha=0.5, label='Actual_100[%]')
+          ax.plot_surface(grid_x, grid_y, Z_pred, color='yellow', alpha=0.5, label='predicted-extrapolated_100[%]')
+          ax.set_xlabel('SubAxes')
+          ax.set_ylabel('MainAxes')
+          ax.set_zlabel('Value')
+          plt.legend()
+          file_name = os.path.join(folder_path, f"test_result_plot_{idx}_100%.png")
+          plt.savefig(file_name, dpi=300)
+          print(f"Saved: {file_name}")
+          
+          wmape1_acc = calculate_wmape(Z_pred.reshape(16,16), gt_dataset[bush_idx*6 + idx])
+          # wmape2_acc = calculate_wmape(Z_pred, gt_dataset[idx])   
+          wmape2_acc = calculate_wmape(Z_pred[:int(16*pred_percengtage),:int(16*pred_percengtage)], gt_dataset[bush_idx*6 + idx][:int(16*pred_percengtage),:int(16*pred_percengtage)])   
+          
+          wmape_full_range_list.append(float(wmape1_acc))
+          wmape_per_percent_list.append(float(wmape2_acc))
+
+        new_row = pd.DataFrame({"stiffness_num": idx+1, "40%": [wmape_per_percent_list[0]], "50%": [wmape_per_percent_list[1]], "60%": [wmape_per_percent_list[2]], "70%": [wmape_per_percent_list[3]], "80%": [wmape_per_percent_list[4]], "100%": [wmape_full_range_list[0]]})
+          
+        result_df = pd.concat([result_df, new_row], ignore_index=True)
         
-        wmape1_acc = calculate_wmape(prediction.reshape(16,16)[:8,:8], gt_dataset[idx][:8,:8])
-        # wmape2_acc = calculate_wmape(Z_pred, gt_dataset[idx])   
-        wmape2_acc = calculate_wmape(Z_pred[:8,:8], gt_dataset[idx][:8,:8])   
+    
+    mean_row =  pd.DataFrame({"stiffness_num": 'mean', "40%": [result_df['40%'].mean()], "50%": [result_df['50%'].mean()], "60%": [result_df['60%'].mean()], "70%": [result_df['70%'].mean()], "80%": [result_df['80%'].mean()], '100%': [result_df['100%'].mean()]})
+    result_df = pd.concat([result_df, mean_row], ignore_index=True)
+    result_df.to_csv(os.path.join(save_path, f"result_df_bush[{bush_idx}].csv"), index=False)
+    
+    #     predictions_post.append(Z_pred)
+    #     predictions.append(prediction)
         
-        wmape1_acc_total += wmape1_acc
-        wmape2_acc_total += wmape2_acc
-        
-        predictions_post.append(Z_pred)
-        predictions.append(prediction)
-        
-    predictions = np.vstack(predictions)  # (N, field_range)
-    predictions_post = np.vstack(predictions_post)
+    # predictions = np.vstack(predictions)  # (N, field_range)
+    # predictions_post = np.vstack(predictions_post)
     
     acc1 = wmape1_acc_total / 6
     acc2 = wmape2_acc_total / 6
     
-    print(f"WMAPE1: {wmape1_acc:.4f}, WMAPE2: {wmape2_acc:.4f}")
+    print(f"WMAPE1: {acc1:.4f}, WMAPE2: {acc2:.4f}")
     return predictions, wmape1_acc, wmape2_acc
 
 
@@ -124,19 +151,27 @@ def test_model(model, device, input_scaler, output_scalers, test_dataset, gt_dat
 
 if __name__ == "__main__":
     # 저장된 경로
-    save_path = r"E:\Dongwoo\TeamWork\Hyundai_bush_2\github\bush_prediction_CNN\results\[1]CNN-Iter0__ep5000_bat64_lr0.0005"
-    model_path = os.path.join(save_path, 'saved_models\\model_test_idx_0_Q.pth' )
-    input_path = './resource/inference/input_data_.xlsx'
-    
-    gt_source = r'E:\Dongwoo\TeamWork\Hyundai_bush_2\github\bush_prediction_CNN\resource\combined_data_10.npy'
-    
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model, input_scaler, output_scalers = load_model_and_scalers(model_path, 12, device)
-    # 디바이스 설정
+    save_path = r"E:\Dongwoo\TeamWork\Hyundai_bush_2\github\bush_prediction_CNN\results\inference_참설계변수"
+    # save_path = r"E:\Dongwoo\TeamWork\Hyundai_bush_2\github\bush_prediction_CNN\results\inference_예측설계변수"
+    # target_bush = ('06_04_NX4', 0)
+    # target_bush = ('06_11_MQ4', 5)
+    # target_bush = ('G_06_04_IK', 13)
+    target_bushes = [('06_04_NX4', 0), ('06_11_MQ4', 5), ('G_06_04_IK', 13) ,('G_11_01_IK', 18)]
+    target_bushes = [('06_04_NX4', 0), ('06_11_MQ4', 5), ('G_06_04_IK', 13) ]
+    for target_bush in target_bushes:
+      # input_path = f'./resource/inference/{target_bush[0]}_.xlsx'
+      input_path = f'./resource/inference/{target_bush[0]}.xlsx'
+      model_path = os.path.join(save_path, f'saved_models\\model_test_idx_{target_bush[1]}.pth' ) # 모델 경로
+      
+      gt_source = r'E:\Dongwoo\TeamWork\Hyundai_bush_2\github\bush_prediction_CNN\resource\combined_data_10.npy'
+      
+      device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+      model, input_scaler, output_scalers = load_model_and_scalers(model_path, 12, device)
+      # 디바이스 설정
 
-    # 데이터셋 로드 (이 부분은 데이터셋 구조에 맞게 수정 필요)
-    dataset = VEPDataset_inference(input_path = input_path, field_range=256, num_stiffness=6)
-    test_dataloder = dataset.get_loader(input_scaler=input_scaler)
-    dataset_gt = VEPDataset(batch =1 , output_path = gt_source, gt_path= gt_source, field_range=256, num_stiffness=6)
+      # 데이터셋 로드 (이 부분은 데이터셋 구조에 맞게 수정 필요)
+      dataset = VEPDataset_inference(input_path = input_path, field_range=256, num_stiffness=6)
+      test_dataloder = dataset.get_loader(input_scaler=input_scaler)
+      dataset_gt = VEPDataset(batch =1 , output_path = gt_source, gt_path= gt_source, field_range=256, num_stiffness=6)
 
-    prediction, acc, acc_post = test_model(model, device, input_scaler, output_scalers, test_dataloder, gt_dataset=dataset_gt, field_range=256, save_path=save_path)
+      prediction, acc, acc_post = test_model(model, device, input_scaler, output_scalers, test_dataloder, gt_dataset=dataset_gt, target_bush= target_bush, field_range=256, save_path=save_path)
